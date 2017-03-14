b2211a3d982cacf154472284bfd77fc59bc91632
Sergey Archipov <sergey.archipov.jp@gmail.com>
2017-03-14 09:31:17 +0200

    Init commit

---
+++ src/main/java/lt/vtvpmc/archipov/App.java
@@ -0,0 +1,17 @@
+package lt.vtvpmc.archipov;
+
+import java.util.UUID;
+
+public class App {
+    public static void main(String[] args) {
+
+        App obj = new App();
+        System.out.println("Unique ID : " + obj.generateUniqueKey());
+    }
+
+    public String generateUniqueKey(){
+
+        String id = UUID.randomUUID().toString();
+        return id;
+    }
+}
